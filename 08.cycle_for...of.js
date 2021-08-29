e19b5ae3b19e6a52c4ef41c265fa85d4df44b8d5
rgm-b <rgm.b@yandex.ru>
2021-08-29 21:18:46 +0300

    цикл for...of

---
+++ 08.cycle_for...of.js
@@ -0,0 +1,17 @@
+const array = [1, 2, 3, 4, 5];
+
+// for (let i=0; i<array.length; i++){
+//     console.log(array[i]);
+// }
+
+// array.forEach( function(item){
+//     console.log(item);
+// });
+
+// for(let item of array){
+//     console.log(item);
+// }
+
+for (let item of 'ABCDEFG'){
+    console.log(item);
+}
